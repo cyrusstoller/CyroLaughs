b303c46e46b7211929b4fb869d284a6fc2ec7be3
Cyrus Stoller <cyrus.stoller@gmail.com>
2012-03-31 15:55:14 -0700

    Initial commit with devise user, spork, autotest, and rspec

---
+++ app/models/user.rb
@@ -0,0 +1,58 @@
+# == Schema Information
+# Schema version: 20120331223648
+#
+# Table name: users
+#
+#  id                     :integer         not null, primary key
+#  email                  :string(255)     default(""), not null
+#  encrypted_password     :string(255)     default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer         default(0)
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string(255)
+#  last_sign_in_ip        :string(255)
+#  password_salt          :string(255)
+#  confirmation_token     :string(255)
+#  confirmed_at           :datetime
+#  confirmation_sent_at   :datetime
+#  unconfirmed_email      :string(255)
+#  failed_attempts        :integer         default(0)
+#  unlock_token           :string(255)
+#  locked_at              :datetime
+#  authentication_token   :string(255)
+#  username               :string(255)
+#  admin                  :boolean
+#  created_at             :datetime        not null
+#  updated_at             :datetime        not null
+#
+
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :token_authenticatable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+  # attr_accessible :title, :body
+  
+  #to allow a user to sign in using either thier username or email
+  attr_accessor :login
+  attr_accessible :login
+
+  def display_name
+    return username unless username.nil? or username.blank?
+    return email.split("@")[0]
+  end
+  
+  protected    
+
+  def self.find_for_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    login = conditions.delete(:login)
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CyroLaughs::Application.config.secret_token = '1861179a8d645dc224d5c68c941b6d9f8012feb4f05c0e33891e20f2876a171cf13d6a9426db410cb201cd81290e2445ec70d955d26dbe05416b4cb60a47eed9'
