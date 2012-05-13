acff85d2a07a4603147ca9b163e7327ede6e163f
Cyrus Stoller <cyrus.stoller@gmail.com>
2012-05-12 22:35:35 -0700

    Finished the pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,48 @@
 class PagesController < ApplicationController
   def home
-    redirect_to about_path
+    flash.keep
+    redirect_to video_path(get_queued_video, :g => SecureRandom.hex(10))
   end
-
+  
   def about
+    @title = "About"
   end
-
+  
   def recent
+    @title = "Recently watched"
+    
+    if signed_in?    
+      @videos = current_user.videos_watched.order("session_watch_histories.updated_at DESC, session_watch_histories.created_at DESC").
+        paginate(:page => params[:page])
+    else
+      @videos = Video.joins("INNER JOIN session_watch_histories ON session_watch_histories.video_id = videos.id").
+        where("session_watch_histories.session_id = :session_id", :session_id => session[:session_id]).
+        order("session_watch_histories.updated_at DESC, session_watch_histories.created_at DESC").paginate(:page => params[:page])
+    end
+    
+    if @videos.empty?
+      flash[:notice] = "You haven't watched any movies yet"
+      redirect_to get_queued_video
+      return
+    end
   end
-
+  
   def liked
+    @title = "Recently liked"
+    
+    if signed_in?
+      @videos = current_user.videos_liked.order("session_watch_histories.updated_at DESC, session_watch_histories.created_at DESC").
+        paginate(:page => params[:page])
+    else
+      @videos = Video.joins("INNER JOIN session_watch_histories ON session_watch_histories.video_id = videos.id").
+        where("session_watch_histories.session_id = :session_id AND session_watch_histories.status = #{APP_CONFIG["Awesome"]}", :session_id => session[:session_id]).
+        order("session_watch_histories.updated_at DESC, session_watch_histories.created_at DESC").paginate(:page => params[:page])
+    end
+    
+    if @videos.empty?
+      flash[:notice] = "You haven't liked any movies yet"
+      redirect_to get_queued_video
+      return
+    end
   end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,10 +3,18 @@ require 'spec_helper'
 describe PagesController do
   render_views
   
+  before(:each) do
+    @video = Factory(:video)
+  end
+  
+  def create_watch_history(session_id)
+    SessionWatchHistory.create!(:session_id => session_id, :video_id => @video.id, :ip_address => "blah", :count => 1, :status => 1)
+  end
+  
   describe "GET 'home'" do
-    it "redirects to about_path" do
+    it "redirects to video_path" do
       get 'home'
-      response.should redirect_to(about_path)
+      response.should be_redirect
     end
   end
 
@@ -18,16 +26,28 @@ describe PagesController do
   end
 
   describe "GET 'recent'" do
-    it "returns http success" do
-      get 'recent'
+    it "returns http success if there is a recently watched video" do
+      create_watch_history("woot")
+      get 'recent', nil, {:session_id => "woot"}
       response.should be_success
+    end
+    
+    it "should redirect if there are no liked videos" do
+      get 'recent'
+      response.should redirect_to(@video)
     end
   end
 
   describe "GET 'liked'" do
-    it "returns http success" do
-      get 'liked'
+    it "returns http success if there is a recently watched video" do
+      create_watch_history("woot")
+      get 'liked', nil, {:session_id => "woot"}
       response.should be_success
+    end
+    
+    it "should redirect if there are no liked videos" do
+      get 'liked'
+      response.should redirect_to(@video)
     end
   end
 
